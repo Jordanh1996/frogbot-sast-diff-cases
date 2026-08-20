9537348550261d8f1dd561515be0618b47d9159d
Jordan Hury <jordanhuri96@gmail.com>
2026-08-20 10:31:38 +0300

    case-06: benign addition to Middle off the flow path

--- src/main/java/com/cases/Middle.java
+++ src/main/java/com/cases/Middle.java
@@ -1,6 +1,10 @@
 package com.cases;
 
 public class Middle {
+    public String describe() {
+        return "forwards export targets to the executor";
+    }
+
     public Process pass(String value) throws Exception {
         return new Sink().exec(value);
     }
